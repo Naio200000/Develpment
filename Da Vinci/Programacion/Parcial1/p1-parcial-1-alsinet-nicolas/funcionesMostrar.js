ae0497deae4873f040c63620b4ffb475f1aa2953
Nicolas Alsinet <nicolas.alsinet@gmail.com>
2022-10-11 19:45:36 -0300

    Se agrega la funcion mostrar Discos

---
+++ Da Vinci/Programacion/Parcial1/p1-parcial-1-alsinet-nicolas/funcionesMostrar.js
@@ -0,0 +1,73 @@
+function generarDiscos() {
+    let html = ``;
+    let divDisco = ``;
+    let divPistas = ``;
+    let discoPista = ``;
+    for (let i = 0; i < discos.length; i++) {
+        divDisco = `<h3>${discos[i].Nombre}</h3>
+                <p><b>Autor: </b>${discos[i].Autor}</p>
+                <p><b>Código: </b>${discos[i].Codigo}</p>`;
+        for (let j = 0; j < discos[i].Pistas.length; j++) {
+            if (discos[i].Pistas[j].Duracion < 181 ) {
+
+            
+            divDisco += `<p><b>Pista: </b>${discos[i].Pistas[j].Nombre}</p>
+                    <p><b>Duración: </b>${discos[i].Pistas[j].Duracion}</p>`
+            } else {
+            divDisco += `<p><b>Pista: </b>${discos[i].Pistas[j].Nombre}</p>
+                    <p class="rojo"><b>Duración: </b>${discos[i].Pistas[j].Duracion}</p>`
+
+            }
+        }
+        discoPista += '<div>' + divDisco + '</div><div>'+ divPistas + '</div>';
+    }
+    html = '<div class="info">' + discoPista + '</div>';
+    return html
+}
+let listaDiscos = document.getElementById('disc-list_tabla');
+let discosTotal = document.getElementById('total-disc');
+
+function mostrarDiscos () {
+    listaDiscos.innerHTML = '<tr><th>Codigo</th><th>Disco</th><th>Artista</th></tr>';
+    if (discos.length == 0) {
+        discosTotal.innerHTML = 0;
+
+    } else {
+        discosTotal.innerHTML = discos.length;
+        listarDiscos();
+    }
+}
+
+function listarDiscos () {
+    for (i = 0; i < discos.length; i++) {
+        let table_row = document.createElement('tr');
+        let table_nombre = document.createElement('td');
+        let table_autor = document.createElement('td');
+        let table_codigo = document.createElement('td');
+        // let table_boton = generaBoton(disco[i].Codigo)
+        // console.log(table_boton);
+        table_nombre.innerHTML = discos[i].Nombre;
+        table_autor.innerHTML = discos[i].Autor;
+        table_codigo.innerHTML = discos[i].Codigo;
+        table_row.appendChild(table_codigo);
+        table_row.appendChild(table_nombre);
+        table_row.appendChild(table_autor);
+        // table_row.appendChild(table_boton);
+        listaDiscos.appendChild(table_row)
+    }
+}
+function generaBoton(dato) {
+    let td = document.createElement('td');
+    let boton = document.createElement('button');
+    boton.id = dato
+    boton.innerHTML = `Ver disco`
+    td.appendChild(boton)
+    console.log(td)
+    return td
+}
+mostrarDiscos()
+
+let boton = document.getElementsByTagName('tr')
+boton.addEventListener('click', () =>{
+    
+})
--- Da Vinci/Programacion/Parcial1/p1-parcial-1-alsinet-nicolas/index.js
+++ Da Vinci/Programacion/Parcial1/p1-parcial-1-alsinet-nicolas/index.js
@@ -3,68 +3,69 @@
 /*
  * Alsinet, Nicolas
  */
-// let disco1 = {
-//     Nombre: 'DIsco1',
-//     Autor: 'Yo',
-//     Codigo: 1,
-//     Pistas: [
-//         {
-//             Nombre: 'Pista1',
-//             Duracion: '123',
-//         },
-//         {
-//             Nombre: 'Pista2',
-//             Duracion: '123',
-//         },
-//         {
-//             Nombre: 'Pista3',
-//             Duracion: '123',
-//         }
-//     ]
-// };
-// let disco2 = {
-//     Nombre: 'DIsco2',
-//     Autor: 'Yo',
-//     Codigo: 2,
-//     Pistas: [
-//         {
-//             Nombre: 'Pista1',
-//             Duracion: '123',
-//         },
-//         {
-//             Nombre: 'Pista2',
-//             Duracion: '123',
-//         },
-//         {
-//             Nombre: 'Pista3',
-//             Duracion: '123',
-//         }
-//     ]
-// };
-// let disco3 = {
-//     Nombre: 'DIsco3',
-//     Autor: 'Yo',
-//     Codigo: 3,
-//     Pistas: [
-//         {
-//             Nombre: 'Pista1',
-//             Duracion: '123',
-//         },
-//         {
-//             Nombre: 'Pista2',
-//             Duracion: '123',
-//         },
-//         {
-//             Nombre: 'Pista3',
-//             Duracion: '123',
-//         }
-//     ]
-// };
+let disco1 = {
+    Nombre: 'DIsco1',
+    Autor: 'Yo',
+    Codigo: 1,
+    Pistas: [
+        {
+            Nombre: 'Pista1',
+            Duracion: '123',
+        },
+        {
+            Nombre: 'Pista2',
+            Duracion: '123',
+        },
+        {
+            Nombre: 'Pista3',
+            Duracion: '123',
+        }
+    ]
+};
+let disco2 = {
+    Nombre: 'DIsco2',
+    Autor: 'Yo',
+    Codigo: 2,
+    Pistas: [
+        {
+            Nombre: 'Pista1',
+            Duracion: '123',
+        },
+        {
+            Nombre: 'Pista2',
+            Duracion: '123',
+        },
+        {
+            Nombre: 'Pista3',
+            Duracion: '123',
+        }
+    ]
+};
+let disco3 = {
+    Nombre: 'DIsco3',
+    Autor: 'Yo',
+    Codigo: 3,
+    Pistas: [
+        {
+            Nombre: 'Pista1',
+            Duracion: '123',
+        },
+        {
+            Nombre: 'Pista2',
+            Duracion: '123',
+        },
+        {
+            Nombre: 'Pista3',
+            Duracion: '123',
+        }
+    ]
+};
+
 // Ejemplo de la estructura de un disco:
 
 let codigos = [];
 let pistas = [];
-let discos = [];
+let discos = [disco1, disco2, disco3];
 
 // Función Cargar:
 /**
@@ -73,15 +74,16 @@ let discos = [];
  * @returns Array de discos 
  * Faltaría validar si apretó por error.
  */
-const Cargar = () => {
+function Cargar() {
     let nombre = validaTexto('Ingrese el Nombre del Disco', true);
     let autor = validaTexto('Ingrese el Nombre del Autor');
     let codigo = pedirCodigo();
     let pistas = armarPistas();
     let disco = armarDisco(nombre, autor, codigo, pistas);
     agregarDisco(disco);
+    mostrarDiscos();
     return discos;
-};
+}
 
 // Función Mostrar:
 const Mostrar = () => {
@@ -95,30 +97,4 @@ const Mostrar = () => {
 
 // Todas las funciones que necesites:
 
-function generarDiscos() {
-    let html = ``;
-    let divDisco = ``;
-    let divPistas = ``;
-    let discoPista = ``;
-    for (let i = 0; i < discos.length; i++) {
-        divDisco = `<h3>${discos[i].Nombre}</h3>
-                <p><b>Autor: </b>${discos[i].Autor}</p>
-                <p><b>Código: </b>${discos[i].Codigo}</p>`;
-        for (let j = 0; j < discos[i].Pistas.length; j++) {
-            if (discos[i].Pistas[j].Duracion < 181 ) {
-
-            
-            divDisco += `<p><b>Pista: </b>${discos[i].Pistas[j].Nombre}</p>
-                    <p><b>Duración: </b>${discos[i].Pistas[j].Duracion}</p>`
-            } else {
-            divDisco += `<p><b>Pista: </b>${discos[i].Pistas[j].Nombre}</p>
-                    <p class="rojo"><b>Duración: </b>${discos[i].Pistas[j].Duracion}</p>`
-
-            }
-        }
-        discoPista += '<div>' + divDisco + '</div><div>'+ divPistas + '</div>';
-    }
-    html = '<div class="info">' + discoPista + '</div>';
-    return html
-}
 
