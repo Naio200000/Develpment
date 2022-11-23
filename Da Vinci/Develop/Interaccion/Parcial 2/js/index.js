e2e0a947be8bd4e6a2ff0745325b7419720ba413
Nicolas Alsinet <nicolas.alsinet@gmail.com>
2022-11-23 19:05:28 -0300

    cambios en los modales

--- Da Vinci/Develop/Interaccion/Parcial 2/js/index.js
+++ Da Vinci/Develop/Interaccion/Parcial 2/js/index.js
@@ -71,7 +71,7 @@ class Producto {
         productoNombre.setAttribute('type', 'button');
         productoNombre.setAttribute('class', 'btn btn-primary card-title');
         productoNombre.setAttribute('data-bs-toggle', 'modal');
-        productoNombre.setAttribute('data-bs-target', '#staticBackdrop')
+        productoNombre.setAttribute('data-bs-target', '#ProductoModal')
         productoNombre.id = `${this.id}boton_mostrar`;
         productoNombre.addEventListener('click', (e) => {
             let boton = e.target;
@@ -110,8 +110,25 @@ class Producto {
         col.append(productoCard);
         return col;
     }
+    agregarBotonModal() {
+        let boton = d.getElementById('botonProducto').children[1];
+        if (boton.id == 'cambiarIDBoton') {
+            boton.innerHTML = `Agregar <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16"><path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/><path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/></svg>`;
+            boton.addEventListener('click', (e) => {
+                let boton = e.target;
+                for (let i = 0; i < productos.length; i++) {
+                    if (parseInt(boton.id) == productos[i].id) {
+                        carrito.agregar(productos[i])
+                        break
+                    }
+                }
+                mostrarMiniCarrito();
+            })
+        }
+        boton.id = `${this.id}productoboton`;
+    }
     agregarDatosModal() {
-
+        d.getElementById('tituloProducto').innerHTML = this.nombre
         let carrousel = d.getElementById('carrusel-producto').children;
         for (let i = 0; i < carrousel.length; i++) {
             carrousel[i].innerHTML = '';
@@ -124,20 +141,8 @@ class Producto {
         d.getElementById('categoriaProducto').innerHTML = this.categoria;
         d.getElementById('descripProducto').innerHTML = this.descrip_larga;
         d.getElementById('precioProducto').innerHTML = this.precio;
-        let boton = d.getElementById('botonProducto')
-        boton.id = `${this.id}productoboton`;
-        boton.innerHTML = `Agregar <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16"><path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/><path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/></svg>`;
-        boton.addEventListener('click', (e) => {
-            let boton = e.target;
-            for (let i = 0; i < productos.length; i++) {
-                if (parseInt(boton.id) == productos[i].id) {
-                    carrito.agregar(productos[i])
-                    break
-                }
-            }
-            mostrarMiniCarrito();
-        })
-    
+        d.getElementById('precioCuotas').innerHTML = (this.precio / 6).toFixed(2);
+        this.agregarBotonModal();
     }
 };
 
